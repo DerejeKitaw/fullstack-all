842b177ef3de005a2eb085abec303e8592f3651b
Thomas Kelly <tmkelly28@gmail.com>
2018-07-12 11:02:51 -0400

    add moar front end

--- 01-junior/curriculum-review/pug-party/server/api/parties.js
+++ 01-junior/curriculum-review/pug-party/server/api/parties.js
@@ -1,2 +1,15 @@
 const router = require('express').Router()
+const {Party, Pug} = require('../db')
 module.exports = router
+
+// GET /api/parties
+router.get('/', async (req, res, next) => {
+  try {
+    const parties = await Party.findAll({
+      include: [{model: Pug, as: 'host'}]
+    })
+    res.json(parties)
+  } catch (err) {
+    next(err)
+  }
+})
--- 01-junior/curriculum-review/pug-party/server/api/pugs.js
+++ 01-junior/curriculum-review/pug-party/server/api/pugs.js
@@ -1,11 +1,21 @@
 const router = require('express').Router()
-const {Pug} = require('../db')
+const {Op} = require('sequelize')
+const {Pug, Party} = require('../db')
 module.exports = router
+
+// db.model('pug_party')
 
 // GET /api/pugs
 router.get('/', async (req, res, next) => {
   try {
-    const pugs = await Pug.findAll()
+    const pugs = await Pug.findAll({
+      where: {
+        age: {
+          [Op.gte]: req.query.minage
+        }
+      },
+      include: [{model: Party}]
+    })
     res.json(pugs)
   } catch (err) {
     next(err)
@@ -16,18 +26,49 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const newPug = await Pug.create(req.body)
-    res.json(newPug)
+    res.status(201).json(newPug)
   } catch (err) {
     next(err)
   }
 })
 
+router.use('/:pugId', async (req, res, next) => {
+  try {
+    const pug = await Pug.findById(req.params.pugId)
+    if (!pug) {
+      const err = new Error('Pug not found!')
+      err.status = 404
+      return next(err)
+    }
+    req.pug = pug
+    next()
+  } catch (err) {
+    next(err)
+  }
+})
+
+// GET /api/pugs/:pugId
+router.get('/:pugId', async (req, res, next) => {
+  res.json(req.pug)
+})
+
 // PUT /api/pugs/:pugId
 router.put('/:pugId', async (req, res, next) => {
   try {
-    const pug = await Pug.findById(req.params.pugId)
-    const updatedPug = await pug.update(req.body)
+    const updatedPug = await req.pug.update({
+      age: req.body.age
+    })
     res.json(updatedPug)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// DELETE /api/pugs/:pugId
+router.delete('/:pugId', async (req, res, next) => {
+  try {
+    await req.pug.destroy()
+    res.sendStatus(204)
   } catch (err) {
     next(err)
   }
